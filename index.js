fb6111bef4827b89d6fcaf270b2c6ed7225b236c
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-10-28 13:33:19 +0000

    endpoint working for user fetch

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const express = require("express");
 const cors = require("cors");
+const db = require("./data/db");
 
 const server = express();
 
 server.use(cors());
 server.use(express.json());
 
-server.get("*", handleRequest);
+server.get("/api/users", getAllUsers);
+server.get("*", handleDefault);
 
-function handleRequest(req, res) {
+function getAllUsers(req, res) {
+  db.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      res.status(500).json({
+        success: false,
+        message: "The users information could not be retrieved."
+      });
+    });
+}
+
+function handleDefault(req, res) {
   res.json("it seems to be working");
 }
 
-server.listen((process.env.PORT || 3000), () => {
+server.listen(process.env.PORT || 3000, () => {
   console.log("listening on " + (process.env.PORT || 3000));
 });
