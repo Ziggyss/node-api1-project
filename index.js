6d1d092a59059e731d73b3958d7b1a5fc1d07178
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-10-28 13:06:16 +0000

    set up project and test initial response

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// implement your API here
+const express = require("express");
+const cors = require("cors");
+
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+
+server.get("*", handleRequest);
+
+function handleRequest(req, res) {
+  res.json("it seems to be working");
+}
+
+server.listen((process.env.PORT || 3000), () => {
+  console.log("listening on " + (process.env.PORT || 3000));
+});
